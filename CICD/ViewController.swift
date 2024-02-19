648ab4716bcd00860f7dfe6b60ce8b5051059ede
“Sesadev” <pradhanjitu86@gmail.com>
2024-02-19 17:20:52 +0530

    initial commit

---
+++ CICD/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CICD
+//
+//  Created by Jiten on 19/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func add(_ a: Int, _ b: Int) -> Int {
+           return a + b
+       }
+
+}
+
---
+++ CICDTests/CICDTests.swift
@@ -0,0 +1,20 @@
+//
+//  CICDTests.swift
+//  CICDTests
+//
+//  Created by Jiten on 19/02/24.
+//
+
+import XCTest
+@testable import CICD
+
+final class CICDTests: XCTestCase {
+
+ 
+    func testAddition() {
+        let viewController = ViewController()
+        let result = viewController.add(2, 3)
+        XCTAssertEqual(result, 5, "Addition function should return 5")
+    }
+
+}
